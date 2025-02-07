50908589b0b399e5e3b4a21d5a8db92115ed9650
thanhhuy44 <thanhhuy28062001@gmail.com>
2025-02-07 10:22:41 +0700

    Fix: test husky

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -1,9 +1,4 @@
-import { getServerAuthSession } from "@/utils/auth";
-
 async function Page() {
-  const auth = await getServerAuthSession();
-  console.log("🚀 ~ Page ~ auth:", auth);
-
   return <main>home page</main>;
 }
 
